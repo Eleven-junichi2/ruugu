63d87a9c3f13844ea38cda95f024fd3b34c8e2bd
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-07-06 17:57:44 +0900

    init prototype branch

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,54 @@
 mod models;
 
-use std::{array, collections::HashMap, default::Default};
+use std::{error::Error};
+
+use crossterm::{
+    cursor,
+    event::{Event, KeyCode, KeyEvent},
+    execute,
+    style::Print,
+    terminal,
+};
 
 use models::*;
-use ndarray::{array, Array, ArrayView1};
 
-fn main() {
-    let player = Mob {
-        world_coordinates: WorldCoordinates { x: 2, y: 2 },
-        status: Status {
-            hp: Hp {
-                max: 100,
-                current: 100,
-            },
-            exp: Exp {
-                next: 100,
-                current: 0,
-                carrying: 0,
-            },
-            level: Level {
-                max: 100,
-                current: 1,
-            },
-            strength: Strength {
-                max: 100,
-                current: 3,
-            },
-            defense: Defense {
-                max: 100,
-                current: 0,
-            },
-            agility: Agility {
-                max: 100,
-                current: 3,
-            },
-            resilience: Resilience { factor: 0.1 },
-        },
-        relationship: RelationShip {
-            hostile_to_self_as_enemies_of_self: true,
-            friendly_to_self_as_allies_of_self: true,
-            ..Default::default()
-        },
-    };
-    let mut gameworld: GameWorld = GameWorld::default();
-    let mut mapchip_to_display_dict = HashMap::new();
-    mapchip_to_display_dict.insert(0, ' ');
-    mapchip_to_display_dict.insert(1, '.');
-    gameworld.maps.insert(
-        "start_area".to_string(),
-        WorldMap {
-            path_layer: array![
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            mapchip_layer: array![
-                [1, 1, 1, 1, 1],
-                [1, 1, 1, 1, 1],
-                [1, 1, 1, 1, 1],
-                [1, 1, 1, 1, 1],
-                [1, 1, 1, 1, 1]
-            ],
-            mapchip_to_display_dict
-        },
-    );
-    println!("{}", gameworld.maps["start_area"].render_to_string())
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut stdout = std::io::stdout();
+    terminal::enable_raw_mode()?;
+    execute!(stdout, terminal::EnterAlternateScreen)?;
+    loop {
+        execute!(stdout, cursor::Hide, cursor::MoveTo(0, 0),)?;
+        // --handle events--
+        match crossterm::event::read()? {
+            Event::Key(event) => {
+                execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
+                match event {
+                    KeyEvent {
+                        code: KeyCode::Esc, ..
+                    } => break,
+                    KeyEvent {
+                        code: KeyCode::Left,
+                        ..
+                    } => println!("go left"),
+                    KeyEvent {
+                        code: KeyCode::Up, ..
+                    } => println!("go up"),
+                    KeyEvent {
+                        code: KeyCode::Right,
+                        ..
+                    } => println!("go right"),
+                    KeyEvent {
+                        code: KeyCode::Down,
+                        ..
+                    } => println!("go down"),
+                    _ => println!("{:?}", event),
+                }
+            }
+            _ => {}
+        }
+        // ----
+    }
+    execute!(stdout, cursor::Show, terminal::LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
 }
