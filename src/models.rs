fa2a14c9a598433ccccf702e0445b1d6f8a32a0d
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-07-06 19:59:03 +0900

    make player movement

--- src/models.rs
+++ src/models.rs
@@ -1,21 +1,74 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error};
 
 use ndarray::Array2;
 
-struct Mob {
-    
+pub struct DungeonMapCoordinate {
+    pub x: u32,
+    pub y: u32,
+}
+pub struct Hp {
+    pub max: u32,
+    pub current: u32,
+}
+
+pub struct Exp {
+    pub next: u32,
+    pub current: u32,
+    pub carrying: u32,
+}
+
+pub struct Level {
+    pub max: u32,
+    pub current: u32,
+}
+
+pub struct Strength {
+    pub max: u32,
+    pub current: u32,
+}
+
+pub struct Defense {
+    pub max: u32,
+    pub current: u32,
+}
+
+pub struct Agility {
+    pub max: u32,
+    pub current: u32,
+}
+
+pub struct Status {
+    pub hp: Hp,
+    pub exp: Exp,
+    pub level: Level,
+    pub strength: Strength,
+    pub defense: Defense,
+    pub agility: Agility,
+}
+
+impl Status {
+    fn calc_actual_values(&self) -> Result<(), Box<dyn Error>> {
+        todo!("not implemented")
+    }
+}
+
+pub struct Mob {
+    pub coordinate: DungeonMapCoordinate,
+    pub status: Status,
+    pub appearance_char: char,
 }
 
 struct DungeonMap {
-    path_layer: Array2<usize>,
-    mapchip_layer: Array2<usize>,
-    mapchip_to_display_dict: HashMap<usize, char>,
-    mob_layer: Array2<usize>,
+    pub path_layer: Array2<u32>,
+    pub mapchip_layer: Array2<u32>,
+    pub mapchip_to_display_dict: HashMap<u32, char>,
+    pub mob_layer: Array2<u32>,
 }
 
 struct GameWorld {
-    maps: HashMap<String, DungeonMap>,
-    mobs: HashMap<usize, Mob>
+    pub maps: HashMap<String, DungeonMap>,
+    pub mobs: HashMap<u32, Mob>,
+    pub current_map: String,
 }
 
 #[cfg(test)]
