377fd882c5ebdcbaa7b1c8336e3b8252c16d100c
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-07-06 18:11:56 +0900

    make structs

--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,21 @@
+use std::collections::HashMap;
+
 use ndarray::Array2;
+
+struct Mob {
+    
+}
 
 struct DungeonMap {
     path_layer: Array2<usize>,
+    mapchip_layer: Array2<usize>,
+    mapchip_to_display_dict: HashMap<usize, char>,
+    mob_layer: Array2<usize>,
 }
 
 struct GameWorld {
-    maps: HashMap<String, DungeonMap>
+    maps: HashMap<String, DungeonMap>,
+    mobs: HashMap<usize, Mob>
 }
 
 #[cfg(test)]
