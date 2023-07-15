24fa4c49503e8ee72356b432266fb4f1bd1a761b
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-07-15 23:06:15 +0900

    update impl Worldmap

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use crossterm::{
 use models::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-
+    
     loop {
         if let Event::Key(KeyEvent { code, modifiers, kind, state}) = read()? {
             dbg!(code, modifiers, kind, state);
