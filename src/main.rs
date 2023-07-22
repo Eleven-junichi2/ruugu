f164e0d7d4cb44553ba29bf8c049fd60bf918174
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-07-22 22:43:25 +0900

    rename model.rs to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-mod models;
-
 use std::error::Error;
 
 use crossterm::{
@@ -10,10 +8,12 @@ use crossterm::{
     terminal,
 };
 
-use models::*;
+use ruugu::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    
+    let mut worldmap = WorldMap::from_size(9, 9);
+    worldmap.mapchip_to_display_dict.insert(0, ' ');
+    worldmap.mapchip_to_display_dict.insert(1, '.');
     loop {
         if let Event::Key(KeyEvent { code, modifiers, kind, state}) = read()? {
             dbg!(code, modifiers, kind, state);
